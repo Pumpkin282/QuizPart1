380baf76dc38ffe619a20c457c2b0814c2de822b
Pumpkin282 <>
2021-04-16 23:46:01 +0000

    fixed some inconsistencies and did some debugging

--- main.py
+++ main.py
@@ -1,33 +1,41 @@
 #allow a player to enter their details. authenticate these details to ensure they are an authorised player
-user = [['user1', 'password1'],['user2', 'password2'],['user3', 'password3']] #array for the usernames and passwords
+user = [['user1','password1'],['user2','password2'],['user3','password3']] #array for the usernames and passwords
 userEntry = ''
 tries = 3 #user gets 3 tries to enter their username and password 
-found = False #flag to determinte whether username/password is found
+foundName = False #flag to determinte whether username/password is found
 
 
 while userEntry == '' and tries > 0:
   userEntry = str(input('please enter your username: '))
   for item in range (0, len(user)):
     if userEntry == user[item][0]:
-      print("welcome")
+      print('welcome')
       foundName = True
-      password = input("please enter your password: ")
-      for j in passwords:
-        if password == j:
-          print("you're logged in")
-          found = True
-        else:
-          print("incorrect password")
-      break #Not sure about this - is it worth keeping? 
-    else:
-      print("go away")
-      tries += 1
+      passwordEntry = str(input('please enter your password: ')) 
+      
+      if passwordEntry == user [item][1]:  #this is pulling up an error 
+          print('you are logged in')
+      else:
+        print('incorrect password')
+        userEntry = ''
+    if foundName == False:
+      print('go away')
+      tries -= 1
+      userEntry = ''
+        
+          
+      
+
+
+
+
+
 
 import random
 
 from random import *
 
-songs = open("songs","r")
+songs = open('songs','r')
 
 songs_list = songs.read().splitlines()
 
